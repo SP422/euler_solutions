b415e4922f5e564cb02a746513f8e84c7605be44
SP422 <shawnpaul.frias@gmail.com>
2015-05-27 06:00:49 -0700

    Problem 1 complete

---
+++ euler_solutions.js
@@ -0,0 +1,16 @@
+// Problem 1 Solution
+var sum = 0;
+for (var i = 0; i < 1000; i++)
+{
+    if (i % 3 === 0)
+    {
+        sum += i;
+    }
+    else if (i % 5 === 0)
+    {
+        sum += i;
+    }    
+}
+console.log(sum);
+
+// Problem 2 Solution
